ea7ce204bafc2a140db692b44b9070affbe30989
shouny <shooouuun@gmail.com>
2025-08-03 19:48:38 +0300

    ref rules in even-game

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -7,6 +7,8 @@ from .constants import (
     EVEN_ANSWER_NO
 )
 
+RULES = RULES_EVEN 
+
 def get_round_data():
     number = random.randint(MIN_EVEN_NUMBER, MAX_EVEN_NUMBER)
     correct_answer = EVEN_ANSWER_YES if number % 2 == 0 else EVEN_ANSWER_NO
